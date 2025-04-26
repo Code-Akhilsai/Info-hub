0086321a3386761c61ea36b36523535b43b2d6cc
Akhilsai <codeakhilsai@gmail.com>
2025-04-26 07:10:43 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
         <Route path={"/clubs"} element={<Clubs />}></Route>
         <Route path={"/gallery"} element={<Gallery />}></Route>
       </Routes>
+      {/* */}
     </>
   );
 }
