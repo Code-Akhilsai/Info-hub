f18c02f84c72c36fc07900e94ce80a8b6551b00b
Akhilsai <codeakhilsai@gmail.com>
2025-07-09 11:34:39 +0530

    navbar added

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,8 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./cstyles.module.css";
 import { FiMenu } from "react-icons/fi";
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
 
 const Menu = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerOpen(false);
+  };
   const nav = useNavigate();
   const handleClick = () => {
     nav("/signup");
@@ -44,7 +57,63 @@ const Menu = () => {
             Login
           </button>
         </div>
-        <FiMenu size={23} id={styles.icon} />
+        <FiMenu
+          size={23}
+          id={styles.icon}
+          onClick={toggleDrawer}
+          className={styles.burgericon}
+        />
+
+        {/* Nav Drawer */}
+        {isDrawerOpen && (
+          <div className={styles.drawer}>
+            <IoMdClose
+              size={28}
+              onClick={closeDrawer}
+              className={styles.closeicon}
+            />
+
+            <a href="/" className={styles.drawerlink} onClick={closeDrawer}>
+              Home
+            </a>
+            <a
+              href="/clubs"
+              className={styles.drawerlink}
+              onClick={closeDrawer}
+            >
+              Clubs
+            </a>
+            <a
+              href="/about"
+              className={styles.drawerlink}
+              onClick={closeDrawer}
+            >
+              About
+            </a>
+            <a
+              href="/gallery"
+              className={styles.drawerlink}
+              onClick={closeDrawer}
+            >
+              Gallery
+            </a>
+
+            <div className={styles.Buttons2}>
+              <button
+                className={styles.TransparentButton2}
+                onClick={handleClick}
+              >
+                Signup
+              </button>
+              <button
+                className={styles.TransparentButton2}
+                onClick={loginClick}
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
